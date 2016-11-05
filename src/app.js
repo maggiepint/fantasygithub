17980e6d647354f60ece700a80a49301ac0a7993
Maggie Pint <magpint@microsoft.com>
2016-11-05 14:27:27 -0700

    this app works!

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var GitHubApi = require("github");
+
+const app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Fantasy GitHub'})
+});
+
+app.post('/', function (req, res) {
+  var repos = req.body.repo;
+  var token = req.body.token;
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
---
+++ src/statisticAggregator.js
@@ -0,0 +1,34 @@
+const GitHubApi = require('github');
+
+function statisticAggregator(token, repos) {
+  this.github = new GitHubApi({
+    // optional args
+    debug: true,
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    pathPrefix: "/api/v3", // for some GHEs; none for GitHub
+    headers: {
+      "user-agent": "FantasyGitHub" // GitHub is happy with a unique user agent
+    },
+    timeout: 5000
+  });
+  // user token
+  this.github.authenticate({
+    type: "token",
+    token: "userToken",
+  });
+
+  this.repos = repos;
+};
+
+
+function repository(gitHub, repo) {
+  this.github = gitHub;
+  this.owner = repo.split('/')[0];
+  this.repo = repo.split('/')[1];
+}
+
+repository.prototype.getCommits(startDate, endDate) {
+  return github.repo.getCommits({ owner: this.owner, repo: this.repo, since: startDate, until: endDate });
+}
+
