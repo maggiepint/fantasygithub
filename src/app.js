d5092d66c2d2fcc33616c425f5d7e2afab307c59
Geoffrey Grosenbach <boss@topfunky.com>
2016-11-05 15:19:01 -0700

    Serve static files from `public`

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+app.use(express.static('public'));
 
 app.set('view engine', 'pug');
 
